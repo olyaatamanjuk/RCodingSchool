2cede790dcf970c5a3a67a182d7946f72c8bf75c
olyaatamanjuk <olyaatamanjuk@gmail.com>
2017-09-23 19:14:36 +0300

    Add Index view

--- RCodingSchool/Models/CodingSchoolContext.cs
+++ RCodingSchool/Models/CodingSchoolContext.cs
@@ -11,5 +11,8 @@ namespace RCodingSchool.Models
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Teacher> Teachers { get; set; }
-    }
+		public DbSet<Student> Students { get; set; }
+		public DbSet<Topic> Topics { get; set; }
+		public DbSet<Chapter> Chapters { get; set; }
+	}
 }
